Scaling (maybe) fixed on fourier transform

emergence_comparisons.py
@@ -39,7 +39,7 @@ if False:
 paths = ['./Data_15/', './Data_50/', './Data_150/']
 titles = ['Strat ratio 15', 'Strat ratio 50', 'Strat ratio 150']
 
-nstrats = 3
+nstrats = 1
 
 for strat_flag in range(nstrats):   #do unstratified (top) and stratified (bottom)
     print('Dealing with field number', strat_flag)
@@ -66,6 +66,7 @@ for strat_flag in range(nstrats):   #do unstratified (top) and stratified (botto
     xv = np.linspace(-130,130,ncells)
     yv = np.linspace(-130,130,ncells)
     zv = np.linspace(-25,100,ncells)
+
     X, Y = np.meshgrid(xv, yv, indexing='ij')
     # Flatten the grid arrays to form the input to the interpolator
     points = np.vstack([X.ravel(), Y.ravel()]).T
@@ -82,23 +83,68 @@ for strat_flag in range(nstrats):   #do unstratified (top) and stratified (botto
     # In[71]:
     #get curl
 
-    #calculate the winding gauge
+    #calculate the winding gauge. Figure out if this actually works...
+    #z component seems a bit wrong
+    z_photo = int((ncells)*(10.0 - zv[0])/(zv[-1] - zv[0]))
+
     AField = flt.getAFastSingle(bField)
 
+    bField_test = flt.curl(AField,grid_spacing)
+
+
     #calculate the winding gauge for the unit speed field
-    usf = flt.unitSpeedField(bField,0.01)
-    AUnit= flt.addDivergenceCleaningTerm(usf,grid_spacing)
+    usf = flt.unitSpeedField(bField.copy(),0.01)  #transforms bz to be 'unit speed' in the z direction
+    BUnit = flt.addDivergenceCleaningTerm(usf,grid_spacing)   #Returns unit speed field. Makes some sense... But why has BField changed?
+    AWind = flt.getAFastSingle(BUnit)
 
-    AWind = flt.getAFastSingle(AUnit)
-
-    bField = flt.createSingleField(bx,by,bz)
     curlField= flt.curl(bField,grid_spacing)
 
-    # add constant component of A as there be net flux
+    '''
+    for i in range(3):
+        comp = bField_test[:,:,z_photo, i].T/bField[:,:,z_photo, i].T
 
+    for i in range(3):
+        im = axes[2,i].pcolormesh(BUnit[:,:,z_photo, i].T)
+        fig.colorbar(im, ax=axes[2,i])
+        axes[2,i].set_title('Unit Speed Field')
+        im = axes[3,i].pcolormesh(curlField[:,:,z_photo, i].T)
+        fig.colorbar(im, ax=axes[3,i])
+        axes[3,i].set_title('Curlfield')
+
+    fig.tight_layout()
+    plt.savefig('extra_plots/inputs.png')
+    plt.show()
+    plt.close()
+    '''
+
+    # add constant component of A as there be net flux
     bzConst = np.sum(bz[:,:,0])/(dA*(ncells-1)*(ncells-1))
     AConst = flt.AConst(bzConst,points,dA,[ncells, ncells, ncells])
     AField = AField + AConst
+
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(10, 7))
+    for i in range(3):
+        im = axes[0,i].pcolormesh(bField[:,:,z_photo, i].T)
+        fig.colorbar(im, ax=axes[0,i])
+        axes[0,i].set_title('Original BField')
+        print(i, np.max(bField[:,:,z_photo, i].T))
+        im = axes[1,i].pcolormesh(bField_test[:,:,z_photo, i].T)
+        fig.colorbar(im, ax=axes[1,i])
+        axes[1,i].set_title('Curl of A')
+
+    plt.tight_layout()
+    plt.show()
+
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(10, 7))
+
+    for i in range(3):
+        comp = np.abs(bField[ncells//4:3*ncells//4,ncells//4:3*ncells//4,z_photo, i].T/bField_test[ncells//4:3*ncells//4,ncells//4:3*ncells//4,z_photo, i].T)
+        print(i, np.percentile(comp,50))
+        im = axes[1,i].pcolormesh(comp, vmax = 2.0, vmin = 0.0)
+        fig.colorbar(im, ax=axes[1,i])
+
+        axes[1,i].set_title('Comparison')
+    plt.show()
 
     # In[72]:
 
@@ -106,10 +152,10 @@ for strat_flag in range(nstrats):   #do unstratified (top) and stratified (botto
 
     testFLHDen = flt.getFLHDenSingle(bField,AField)
 
-    testWindDen = flt.getFLHDenSingle(bField,AWind)
+    testWindDen = flt.getFLHDenSingle(bField,AWind)   #IS THIS RIGHT?
     # twist density
 
-    twistDensity = flt.twistDen(bField,curlField,0.1)
+    twistDensity = flt.twistDen(bField,curlField,1e-6)
 
     # In[73]:
 
@@ -121,6 +167,7 @@ for strat_flag in range(nstrats):   #do unstratified (top) and stratified (botto
         im = axes[i].imshow(bField[:,:,z_photo,i], origin='lower')
         fig.colorbar(im, ax=axes[i])
     fig.tight_layout()
+    plt.suptitle('Magnetic field')
     plt.savefig('extra_plots/0_%d.png' % strat_flag)
 
     plt.close()
@@ -134,6 +181,7 @@ for strat_flag in range(nstrats):   #do unstratified (top) and stratified (botto
         im = axes[i].imshow(twistDensity[:,:,z_photo*(i+1)], origin='lower')
         fig.colorbar(im, ax=axes[i])
     fig.tight_layout()
+    plt.suptitle('Twist Density')
     plt.savefig('extra_plots/1_%d.png' % strat_flag)
     plt.close()
 
@@ -147,6 +195,7 @@ for strat_flag in range(nstrats):   #do unstratified (top) and stratified (botto
         im = axes[i].imshow(testFLHDen[:,:,z_photo*(i+1)].T, origin='lower')
         fig.colorbar(im, ax=axes[i])
     fig.tight_layout()
+    plt.suptitle('FLH Density')
     plt.savefig('extra_plots/2_%d.png' % strat_flag)
 
     plt.close()
@@ -161,6 +210,8 @@ for strat_flag in range(nstrats):   #do unstratified (top) and stratified (botto
         im = axes[i].imshow(testWindDen[:,:,z_photo*(i+1)].T, origin='lower')
         fig.colorbar(im, ax=axes[i])
     fig.tight_layout()
+    plt.suptitle('Winding Density')
+
     plt.savefig('extra_plots/3_%d.png' % strat_flag)
 
     plt.close()
@@ -179,8 +230,8 @@ for strat_flag in range(nstrats):   #do unstratified (top) and stratified (botto
         lz = grid_spacing[2]*ncells
 
         #set number of points used to calculate the distribution from
-        nx = 400
-        ny = 400
+        nx = 100
+        ny = 100
 
         # set a minimum strength of field line cut off
         bCut = 0.01
@@ -225,153 +276,58 @@ for strat_flag in range(nstrats):   #do unstratified (top) and stratified (botto
         twistFs.append(twistF)
 
 # In[44]:
+if False:
+
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
+    for strat_flag in range(nstrats):
+        im = axes[strat_flag].imshow(flhs[strat_flag].T,origin='lower',cmap='seismic')
+        axes[strat_flag].set_title(titles[strat_flag])
+        fig.colorbar(im, ax=axes[strat_flag])
+    plt.suptitle('Field-line helicities')
+    plt.tight_layout()
+    plt.savefig('extra_plots/flh.png')
+    plt.close()
+
+    # In[45]:
+
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
+    for strat_flag in range(nstrats):
+        im = axes[strat_flag].imshow(flws[strat_flag].T,origin='lower',cmap='seismic')
+        axes[strat_flag].set_title(titles[strat_flag])
+        fig.colorbar(im, ax=axes[strat_flag])
+    plt.suptitle('Field-line windings')
+    plt.tight_layout()
+    plt.savefig('extra_plots/flw.png')
+    plt.close()
 
 
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
-for strat_flag in range(nstrats):
-    im = axes[strat_flag].imshow(flhs[strat_flag].T,origin='lower',cmap='seismic')
-    axes[strat_flag].set_title(titles[strat_flag])
-    fig.colorbar(im, ax=axes[strat_flag])
-plt.suptitle('Field-line helicities')
-plt.tight_layout()
-plt.savefig('extra_plots/flh.png')
-plt.close()
-
-# In[45]:
-
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
-for strat_flag in range(nstrats):
-    im = axes[strat_flag].imshow(flws[strat_flag].T,origin='lower',cmap='seismic')
-    axes[strat_flag].set_title(titles[strat_flag])
-    fig.colorbar(im, ax=axes[strat_flag])
-plt.suptitle('Field-line windings')
-plt.tight_layout()
-plt.savefig('extra_plots/flw.png')
-plt.close()
+    # In[46]:
 
 
-# In[46]:
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
+    for strat_flag in range(nstrats):
+        im = axes[strat_flag].imshow(flhBzs[strat_flag].T,origin='lower',cmap='seismic')
+        axes[strat_flag].set_title(titles[strat_flag])
+        fig.colorbar(im, ax=axes[strat_flag])
+    plt.suptitle('Weighted Field-line helicities')
+    plt.tight_layout()
+    plt.savefig('extra_plots/flhw.png')
+    plt.close()
 
 
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
-for strat_flag in range(nstrats):
-    im = axes[strat_flag].imshow(flhBzs[strat_flag].T,origin='lower',cmap='seismic')
-    axes[strat_flag].set_title(titles[strat_flag])
-    fig.colorbar(im, ax=axes[strat_flag])
-plt.suptitle('Weighted Field-line helicities')
-plt.tight_layout()
-plt.savefig('extra_plots/flhw.png')
-plt.close()
+    # In[ ]:
 
 
-# In[ ]:
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
+    for strat_flag in range(nstrats):
+        im = axes[strat_flag].imshow(twistFs[strat_flag].T,origin='lower',cmap='seismic')
+        axes[strat_flag].set_title(titles[strat_flag])
+        fig.colorbar(im, ax=axes[strat_flag])
+    plt.suptitle('Twists')
+    plt.tight_layout()
+    plt.savefig('extra_plots/tw.png')
+    plt.close()
 
 
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
-for strat_flag in range(nstrats):
-    im = axes[strat_flag].imshow(twistFs[strat_flag].T,origin='lower',cmap='seismic')
-    axes[strat_flag].set_title(titles[strat_flag])
-    fig.colorbar(im, ax=axes[strat_flag])
-plt.suptitle('Twists')
-plt.tight_layout()
-plt.savefig('extra_plots/tw.png')
-plt.close()
-
-
-# In[ ]:
-
-
-def getFieldLineMetrics(fileLoc,dimensions,grid_spacing):
-    #open the file
-    file2read = netcdf.NetCDFFile(fileLoc,'r')
-    # read the field variables
-    bxOg =file2read.variables['bx'][:]*1
-    byOg =file2read.variables['by'][:]*1
-    bzOg =file2read.variables['bz'][:]*1
-    # smush to centred grid
-    bx =0.5*(bxOg[:,:,:(bxOg.shape[2]-1)]+bxOg[:,:,1::bxOg.shape[2]])
-    by =0.5*(byOg[:,:(byOg.shape[1]-1)]+byOg[:,1::byOg.shape[1],:])
-    bz =0.5*(bzOg[:(bzOg.shape[0]-1),:,:]+bzOg[1::bzOg.shape[0],:,:])
-
-    #Reshape so its 0= x comp 1 =y comp 2 = z comp 
-    bx = bx.reshape(dimensions).transpose(2,1,0)
-    by = by.reshape(dimensions).transpose(2,1,0)
-    bz = bz.reshape(dimensions).transpose(2,1,0)
-
-    #spatial grids    
-    xv = np.linspace(-10,10,192)
-    yv = np.linspace(-10,10,192)
-    zv = np.linspace(0,20,192)
-    X, Y = np.meshgrid(xv, yv, indexing='ij')
-    # Flatten the grid arrays to form the input to the interpolator
-    points = np.vstack([X.ravel(), Y.ravel()]).T
-    dx = xv[1]-xv[0]
-    dy = yv[1]-yv[0]
-    dz = zv[1]-zv[0]
-    dA = dx*dy
-    grid_spacing = [dx,dy,dz]
-    
-    bField = flt.createSingleField(bx,by,bz)
-    
-    #get curl
-    curlField= flt.curl(bField,grid_spacing)
-
-    #calculate the winding gauge 
-    AField = flt.getAFastSingle(bField)
-
-    #calculate the winding gauge for the unit speed field 
-    usf = flt.unitSpeedField(bField,0.01)
-    AUnit= flt.addDivergenceCleaningTerm(usf,grid_spacing)
-
-    AWind = flt.getAFastSingle(AUnit)
-
-    #add constant component of A as there be net flux
-
-    bzConst = np.sum(bz[:,:,0])/(dA*191*191)
-    AConst = flt.AConst(bzConst,points,dA,[192,192,192])
-    AField = AField + AConst 
-    
-    # calculate the field line helcity and winding densities
-
-    testFLHDen =flt.getFLHDenSingle(bField,AField)
-    testWindDen =flt.getFLHDenSingle(bField,AWind)
-    twistDensity = flt.twistDen(bField,curlField,0.1)
-    
-    # set up the field line tracer
-    
-    grid_spacing = [20/192,20/192,20/192]
-
-    #set domain lengths
-
-    lx = grid_spacing[0]*192
-    ly = grid_spacing[1]*192
-    lz = grid_spacing[2]*192
-
-    #set number of points used to calculate the distribution from
-    nx = 200
-    ny = 200
-
-    # set a minimum strength of field line cut off 
-    bCut = 0.01
-
-    # set z value for anchoring plane (use photosphere z index = 116 here)
-    zv = 0
-    
-    #interpolate the field for tracer
-    BxInterp,ByInterp,BzInterp = flt.getInterpolatedFieldSingle(bField,grid_spacing[0],grid_spacing[1],grid_spacing[2])
-    
-    #calculate and prepare curves
-    fieldLinesList,goodSeeds,seeds = flt.prepareCurves(bField,BxInterp,ByInterp,BzInterp,grid_spacing,[1,19],[1,19],zv,nx,ny,bCut)
-    
-    bz_surface_interp = BzInterp()
-    #interpolate the quantities of interest
-    flhInterp = flt.getInterpolatedQuantity(testFLHDen,grid_spacing)
-    flhBzInterp = flt.getInterpolatedQuantity(testFLHDen*bz[:,:,0],grid_spacing)
-    flwindInterp = flt.getInterpolatedQuantity(testWindDen,grid_spacing)
-    twistInterp = flt.getInterpolatedQuantity(twistDensity,grid_spacing)
-    
-    flh,indexesflh = flt.fieldLineIntegratedQuantity(flhInterp,goodSeeds,fieldLinesList,seeds,nx,ny)
-    flhBz,indexesflh = flt.fieldLineIntegratedQuantity(flhBzInterp,goodSeeds,fieldLinesList,seeds,nx,ny)
-    flw,indexesflw = flt.fieldLineIntegratedQuantity(flwindInterp,goodSeeds,fieldLinesList,seeds,nx,ny)
-    twistF,indexestwist = flt.fieldLineIntegratedQuantity(twistInterp,goodSeeds,fieldLinesList,seeds,nx,ny)
+    # In[ ]:
 